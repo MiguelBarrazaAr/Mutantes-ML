930c75fcbb355b38a2cdee637d19299d73f3b564
Miguel Barraza <correo@miguelbarraza.com.ar>
2022-01-14 09:21:50 -0300

    ultimas pruebas con dynamodb

--- api-handlers/mutant.js
+++ api-handlers/mutant.js
@@ -5,16 +5,48 @@ const forbiddenError = require("./responses/forbiddenError");
 const badRequestError = require("./responses/badRequestError");
 const okey  = require("./responses/okey");
 
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+const ddb = new AWS.DynamoDB({apiVersion: 'latest'});
+
+function dbCreate(dna, isMutant) {
+    const params = {
+        TableName: 'mutant-ml-api-dev-mutants',
+        Item: {
+            dna: {
+                S: dna.toString(),
+            },
+            mutant: {
+                BOOL: isMutant
+            }
+        }
+      };
+
+      console.log(params);
+
+      ddb.putItem(params, function(error, data) {
+        if (error) {
+          console.log("Error", error);
+        } else {
+          console.log("Success", data);
+        }
+      });
+}
+
+
 exports.handler = async (event) => {
     const data = JSON.parse(event.body);
     if(!dnaValidate(data.dna)) {
         // datos incorrectos:
         return badRequestError();
     }
-    
+
     if(isMutant(data.dna)) {
+        dbCreate(data.dna, true);
         return okey();
     } else {
+        dbCreate(data.dna, false);
         return forbiddenError();
     }
 };
